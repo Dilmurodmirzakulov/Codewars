e5a7b650a116972b7988474877a728299040c9ea
Dilmurodmirzakulov <dilmurodmirzakulovdev@gmail.com>
2023-01-22 11:57:00 +0500

    Create phone number format

---
+++ create-phone-number.js
@@ -0,0 +1,9 @@
+function createPhoneNumber(numbers){
+  let string = ''
+  numbers.forEach(x => string = string + x)
+  return string = '(' + string.slice(0,3) + ') ' + string.slice(3, 6) + '-' + string.slice(6)
+}
+
+function createPhoneNumber(numbers){
+  numbers.join('').replace(/(...)(...)(.*)/, '($1) ($2)-($3)')
+}
